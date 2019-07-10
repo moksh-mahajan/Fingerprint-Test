5a1d4a6913bc51ff179247cc5084a34777a35d97
moksh-mahajan <mokshmahajan008@gmail.com>
2019-07-10 11:21:18 +0530

    Support Libraries set to 27

--- app/src/main/java/com/moksh/fingerprintauthentication/MainActivity.kt
+++ app/src/main/java/com/moksh/fingerprintauthentication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.moksh.fingerprintauthentication
 
 import android.content.Context
 import android.content.Intent
-import android.hardware.biometrics.BiometricPrompt
 import android.hardware.fingerprint.FingerprintManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
